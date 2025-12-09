6c64938a35c2847a0001dc4488018e98e61c5918
준혁 <dobby@junhyeogs-MacBook-Air.local>
2025-12-09 16:38:41 +0900

    fix: CORS 에러 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'firebasestorage.googleapis.com', 'shbimg.interparkb2b.co.kr'],
+    domains: ['localhost', 'firebasestorage.googleapis.com', 'shbimg.interparkb2b.co.kr', 'refresh-plus-production.up.railway.app', 'refresh-plus.vercel.app'],
   },
 }
 
